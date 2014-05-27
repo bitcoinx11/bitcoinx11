3ebf5f5f8a4222b0c663091840a28ad67a1db379
bitcoinx11 <bitcoinx11@163.com>
2014-05-28 07:33:02 +0800

    bitcoinx11

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define bitcoinx11-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "bitcoinx11URI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
